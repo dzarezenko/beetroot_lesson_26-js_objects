8217194dead0bea65c285656b1324c9c227fc276
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-11-04 19:49:51 +0200

    Methods

--- js/objects.js
+++ js/objects.js
@@ -5,12 +5,25 @@ let person = {
   education: {
     graduated_year: 2009,
     institution: "VNTU"
+  },
+
+  name: function() {
+    return `${this.first_name} ${this.last_name}`;
+  },
+
+  setName: function(name) {
+    this.first_name = name;
   }
+
 }
 
 console.log(person);
+person.setName("Alexandra");
+console.log(person);
+
 console.log(person["first_name"]);
 console.log(`${person.first_name} ${person.last_name}`);
+console.log("Full Name", person.name());
 
 console.log(person.education.graduated_year);
 
